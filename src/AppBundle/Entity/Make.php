3041133432c7f63245479e94a3e53a42d478478a
Tomasz <tomaszkoz@o2.pl>
2018-09-07 15:18:32 +0200

    Add calss Make

---
+++ src/AppBundle/Entity/Make.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Make
+ *
+ * @ORM\Table(name="make")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MakeRepository")
+ */
+class Make
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Make", type="string", length=255)
+     */
+    private $make;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set make
+     *
+     * @param string $make
+     *
+     * @return Make
+     */
+    public function setMake($make)
+    {
+        $this->make = $make;
+
+        return $this;
+    }
+
+    /**
+     * Get make
+     *
+     * @return string
+     */
+    public function getMake()
+    {
+        return $this->make;
+    }
+}
+
