3a58d13ff9ab4cbb5e08ee55769fe2ca2905fb44
Tomasz <tomaszkoz@o2.pl>
2018-09-10 09:50:11 +0200

    Add Model entity

---
+++ src/AppBundle/Entity/Model.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Model
+ *
+ * @ORM\Table(name="model")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ModelRepository")
+ */
+class Model
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Model", type="string", length=255, unique=true)
+     */
+    private $model;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set model
+     *
+     * @param string $model
+     *
+     * @return Model
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    /**
+     * Get model
+     *
+     * @return string
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+}
+
