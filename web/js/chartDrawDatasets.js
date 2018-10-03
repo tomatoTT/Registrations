d9784fa74f87abeda3156376e8a79eba8660eddc
Tomasz <tomaszkoz@o2.pl>
2018-10-04 00:26:45 +0200

    Definition chartDrawDatasets.js

--- web/js/chartDrawDatasets.js
+++ web/js/chartDrawDatasets.js
@@ -3,7 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function chartDrawDatasets() {
+function chartDrawDatasets(data) {
     
+    var datasetsObjTable = [];
+    
+    for (i=0; i<data.length; i++) {
+        
+    }
+    
+    return datasetsObjTable;
 }
 
