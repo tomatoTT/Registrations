71bf3cf6b51e6f5931baaf7d3892443683f87982
Tomasz <tomaszkoz@o2.pl>
2018-10-03 22:32:35 +0200

    Definition drawChart.js

--- web/js/chartDraw.js
+++ web/js/chartDraw.js
@@ -4,6 +4,50 @@
  * and open the template in the editor.
  */
 
-
+function drawChart (chartId) {
+    var ctx = document.getElementById(chartId);
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'A',
+                yAxisID: 'A',
+                data: units,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)'
+                ],
+                borderWidth: 2
+                },{
+                    label: 'B',
+                    yAxisID: 'B',
+                    data: [5, 10, 7, 15, 20, 10],
+                    backgroundColor: [
+                        'rgba(200, 99, 132, 0)'
+                    ],
+                    borderColor: [
+                        'rgba(200,99,132,1)'
+                    ],
+                    borderWidth: 1                        
+            }]
+        },
+            options: {
+                scales: {
+                    yAxes: [{
+                        id: 'A',
+                        type: 'linear',
+                        position: 'left'
+                    }, {
+                        id: 'B',
+                        type: 'linear',
+                        position: 'right'
+                    }]
+                }
+            }                
+        });
+    }
 
 
