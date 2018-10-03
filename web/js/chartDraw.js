a14539abd2702b097bac3bdbb907b680a6a3b17a
Tomasz <tomaszkoz@o2.pl>
2018-10-03 23:09:22 +0200

    Definition drawChart.js

--- web/js/chartDraw.js
+++ web/js/chartDraw.js
@@ -3,52 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$.getScript('Chart.js', function() {
 
-    function drawChart (chartId) {
-        var ctx = document.getElementById(chartId);
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'A',
-                    yAxisID: 'A',
-                    data: units,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)'
-                    ],
-                    borderWidth: 2
-                    },{
-                        label: 'B',
-                        yAxisID: 'B',
-                        data: [5, 10, 7, 15, 20, 10],
-                        backgroundColor: [
-                            'rgba(200, 99, 132, 0)'
-                        ],
-                        borderColor: [
-                            'rgba(200,99,132,1)'
-                        ],
-                        borderWidth: 1                        
-                    }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        id: 'A',
-                        type: 'linear',
-                        position: 'left'
-                    }, {
-                        id: 'B',
-                        type: 'linear',
-                        position: 'right'
-                    }]
-                }
-            }                
-        });
-    }
-});
+
+function drawChart (chartId, labels) {
+    var ctx = document.getElementById(chartId);
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'A',
+                yAxisID: 'A',
+                data: units,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)'
+                ],
+                borderWidth: 2
+            }, {
+                label: 'B',
+                yAxisID: 'B',
+                data: [5, 10, 7, 15, 20, 10],
+                backgroundColor: [
+                    'rgba(200, 99, 132, 0)'
+                ],
+                borderColor: [
+                    'rgba(200,99,132,1)'
+                ],
+                borderWidth: 1                        
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    id: 'A',
+                    type: 'linear',
+                    position: 'left'
+                }, {
+                    id: 'B',
+                    type: 'linear',
+                    position: 'right'
+                }]
+            }
+        }                
+    });
+}
 
