8c296ba75f63a4a66bbcf9ed160bf985486e53f5
Tomasz <tomaszkoz@o2.pl>
2018-10-12 15:40:35 +0200

    labels.js creation

---
+++ web/js/labels.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function labelsForMainChart(data) {
+    
+    var labels = [];
+    var label = "";
+    
+    for (i=0; i<data.length; i++) {
+        
+        labels.push(label.concat(data[i].month, "/", data[i].year));
+    }
+    
+    return labels;
+}
+
+
