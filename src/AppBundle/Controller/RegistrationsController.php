5b060928d279eca458b05a7b4d6f81dd6936311b
Tomasz <tomaszkoz@o2.pl>
2018-10-12 16:21:07 +0200

    Add RegistrationsController

---
+++ src/AppBundle/Controller/RegistrationsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Registrations controller
+ * 
+ * @Route("registrations")
+ */
+class RegistrationsController extends Controller
+{
+    /**
+     * @Route("/sendData")
+     */
+    public function sendDataAction(Request $request)
+    {
+        $registrationsData = $this->getDoctrine()
+                ->getRepository('AppBundle:Registrations')
+                ->findAll();
+        if ($request->isXmlHttpRequest() || $request->query->get('showJson') == 1) {
+            $jsonData = array();
+            $idx = 0;
+            foreach ($registrationsData as $registrationsDataSingle) {
+                $temp = array(
+                    'make' => $registrationsDataSingle->getMake(),
+                    'regYear' => $registrationsDataSingle->getRegYear(),
+                    'regMonth' => $registrationsDataSingle->getRegMonth(),
+                    'units' => $registrationsDataSingle->getUnits()
+                );
+                $jsonData[$idx++] = $temp;
+            }
+            return new JsonResponse($jsonData);
+        } else {
+            return new Response('<html><body>nie ma jsona</body></html>');
+        }
+    }
+}
