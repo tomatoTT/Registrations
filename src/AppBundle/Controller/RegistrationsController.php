4aa470c699317ca9d960dae58480ba44eac213b5
Tomasz <tomaszkoz@o2.pl>
2018-10-12 16:25:31 +0200

    RegistrationsController and EngineController modyfication

--- src/AppBundle/Controller/RegistrationsController.php
+++ src/AppBundle/Controller/RegistrationsController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Registrations controller
