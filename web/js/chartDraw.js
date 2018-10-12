c8fbcf6771ffc50c09e840747ce7f426a87a931d
Tomasz <tomaszkoz@o2.pl>
2018-10-12 15:15:41 +0200

    Add function chartDrawDatasets() to chartDraw.js

--- web/js/chartDraw.js
+++ web/js/chartDraw.js
@@ -11,29 +11,7 @@ function chartDraw (chartId, labels) {
         type: 'line',
         data: {
             labels: labels,
-            datasets: [{
-                label: 'A',
-                yAxisID: 'A',
-                data: units,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)'
-                ],
-                borderWidth: 2
-            }, {
-                label: 'B',
-                yAxisID: 'B',
-                data: [5, 10, 7, 15, 20, 10],
-                backgroundColor: [
-                    'rgba(200, 99, 132, 0)'
-                ],
-                borderColor: [
-                    'rgba(200,99,132,1)'
-                ],
-                borderWidth: 1                        
-            }]
+            datasets: chartDrawDatasets(data)
         },
         options: {
             scales: {
