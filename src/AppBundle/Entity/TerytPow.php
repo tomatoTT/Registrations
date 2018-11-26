e28d4b8ebcdf27be1b73cf46739ecde523beadd5
Tomasz <tomaszkoz@o2.pl>
2018-11-26 22:49:29 +0100

    Add teryt powiat

---
+++ src/AppBundle/Entity/TerytPow.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TerytPow
+ *
+ * @ORM\Table(name="teryt_pow")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TerytPowRepository")
+ */
+class TerytPow
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Teryt", type="string", length=255)
+     */
+    private $teryt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Powiat", type="string", length=255)
+     */
+    private $powiat;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set teryt
+     *
+     * @param string $teryt
+     *
+     * @return TerytPow
+     */
+    public function setTeryt($teryt)
+    {
+        $this->teryt = $teryt;
+
+        return $this;
+    }
+
+    /**
+     * Get teryt
+     *
+     * @return string
+     */
+    public function getTeryt()
+    {
+        return $this->teryt;
+    }
+
+    /**
+     * Set powiat
+     *
+     * @param string $powiat
+     *
+     * @return TerytPow
+     */
+    public function setPowiat($powiat)
+    {
+        $this->powiat = $powiat;
+
+        return $this;
+    }
+
+    /**
+     * Get powiat
+     *
+     * @return string
+     */
+    public function getPowiat()
+    {
+        return $this->powiat;
+    }
+}
+
