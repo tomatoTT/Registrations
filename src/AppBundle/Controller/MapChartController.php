7247ae9433607686fbcfd8f2a61215cadd6ed628
Tomasz <tomaszkoz@o2.pl>
2018-12-03 22:41:45 +0100

    QueryBuilder for Map controller

--- src/AppBundle/Controller/MapChartController.php
+++ src/AppBundle/Controller/MapChartController.php
@@ -8,7 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-
 /**
  * Map Chart controller
  * 
@@ -32,17 +31,63 @@ class MapChartController extends Controller
             $regYearMax = $myPost['regYearMax'];
             $regMonthMax = $myPost['regMonthMax'];            
             $make = $myPost['make'];*/
+        $make = "JCB";
+        $regYearMin = 2017;
+        $regMonthMin = 2;
+        $regYearMax = 2018;
+        $regMonthMax = 11;        
+        
+        if ($regYearMin === $regYearMax)
+        {
             $qb = $em->createQueryBuilder();
-            $q = $qb->select('r')
+            $q = $qb->select('r.make, r.regYear, r.regMonth, r.units, r.tERYT')
                     ->from('AppBundle:RegTot', 'r')
                     ->where(
                             $qb->expr()->andX(
-                                    $qb->expr()->between('r.regYear', 2017, 2018),
-                                    $qb->expr()->eq('r.make', $qb->expr()->literal("JCB"))
+                                    $qb->expr()->eq('r.regYear', $regYearMin),
+                                    $qb->expr()->between('r.regMonth', $regMonthMin, $regMonthMax),
+                                    $qb->expr()->eq('r.make', $qb->expr()->literal($make))
                                     )
                     )
                     ->getQuery();
             $result = $q->getResult();
+            var_dump($result);
+            return $this->render('@App/MapChart/show_map.html.twig', array(
+            // ...
+            ));
+        }
+        $qb1 = $em->createQueryBuilder();
+        $q1 = $qb1->select('r.make, r.regYear, r.regMonth, r.units, r.tERYT')
+                ->from('AppBundle:RegTot', 'r')
+                ->where(
+                        $qb1->expr()->andX(
+                                $qb1->expr()->eq('r.regYear', $regYearMin),
+                                $qb1->expr()->gte('r.regMonth', $regMonthMin),
+                                $qb1->expr()->eq('r.make', $qb1->expr()->literal($make))
+                                )
+                )
+                ->getQuery();
+        $resultMin = $q1->getResult();
+            
+        $qb2 = $em->createQueryBuilder();
+        $q2 = $qb2->select('r.make, r.regYear, r.regMonth, r.units, r.tERYT')
+                ->from('AppBundle:RegTot', 'r')
+                ->where(
+                        $qb2->expr()->andX(
+                                $qb2->expr()->eq('r.regYear', $regYearMax),
+                                $qb2->expr()->lte('r.regMonth', $regMonthMax),
+                                $qb2->expr()->eq('r.make', $qb2->expr()->literal($make))
+                                )
+                )
+                ->getQuery();
+        $resultMax = $q2->getResult();            
+        $result = array_merge($resultMin, $resultMax);        
+        var_dump($result);
+        
+        return $this->render('@App/MapChart/show_map.html.twig', array(
+            // ...
+        ));
+
             
             /**$result = $em->getRepository('AppBundle:RegTot')->findBy(array('regYear' => $regYear, 'make' => 'NEWHOLLAND'));*/
             
@@ -63,27 +108,10 @@ class MapChartController extends Controller
      * @Route("/showMap")
      */
     public function showMapAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-        $qb = $em->createQueryBuilder();
-            $q = $qb->select('r.make, r.regYear, r.regMonth, r.units, r.tERYT')
-                    ->from('AppBundle:RegTot', 'r')
-                    ->where(
-                            $qb->expr()->andX(
-                                    $qb->expr()->between('r.regYear', 2017, 2018),
-                                    $qb->expr()->eq('r.make', $qb->expr()->literal("JCB"))
-                                    )
-                    )
-                    ->getQuery();
-        $result = $q->getResult();
-        /**$sourceMap = array();
-            foreach ($result as $value) {
-                $sourceMap[] = $value->getRegYear();
-            }*/
-        var_dump($result);
+    {   
         return $this->render('@App/MapChart/show_map.html.twig', array(
             // ...
         ));
     }
-
+    
 }
