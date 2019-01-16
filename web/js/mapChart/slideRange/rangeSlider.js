199e10aec41fdcc6b791328fa53677cfb300421e
Tomasz <tomaszkoz@o2.pl>
2019-01-16 19:16:47 +0100

    update

--- web/js/mapChart/slideRange/rangeSlider.js
+++ web/js/mapChart/slideRange/rangeSlider.js
@@ -7,3 +7,13 @@ function rangeSlider(selector, data) {
     });
 }
 
+function rangeArray(data) {
+    if (data[0].yearMin === data[0].yearMax) {
+        var rangeNum = data[0].monthMax - data[0].monthMin + 1;
+        var labels = array();
+        for (var i=0; i<=rangeNum; i++) {
+            
+        }
+    }
+}
+
