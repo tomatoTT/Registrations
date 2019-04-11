db671b5fea1b00fbedfe33e1e30feaa1b3677b8f
Tomasz <tomaszkoz@o2.pl>
2019-04-11 23:54:31 +0200

    Add hover to map chart

--- web/js/mapChart/hover/mapChartHover.js
+++ web/js/mapChart/hover/mapChartHover.js
@@ -1,4 +1,17 @@
 function mapChartHover()  {
-    $(".st0, .st1").tooltip();
+    $(".st0, .st1").hover(
+        function(e) {
+            let county = $(this).data("county");
+            $("body").append('<p id="tooltip">'+county+'</p>');
+            
+            $('#tooltip').css({
+                left:  e.pageX,
+                top:   e.pageY
+            }); 
+        },
+        function() {
+            $('#tooltip').remove();
+        }
+            );
 }
 
